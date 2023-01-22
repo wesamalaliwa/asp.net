bbe9a044d1a19443bc22238cab2623d7ce93e70b
wesam abu shaqra <80597414+wesamalaliwa@users.noreply.github.com>
2023-01-22 23:39:26 +0300

    Add files via upload

---
+++ task1.aspx
@@ -0,0 +1,168 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="task1.aspx.cs" Inherits="ASP.NETEntityFramework.task1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
+    <style>
+        .ss{
+            text-align:center;
+        }
+        .ListGridViewHeader
+
+{
+
+ text-align:center;
+}
+        body{
+            background-color: #e5e5f7;
+opacity: 0.8;
+background-image:  radial-gradient(#444cf7 0.5px, transparent 0.5px), radial-gradient(#444cf7 0.5px, #e5e5f7 0.5px);
+background-size: 20px 20px;
+background-position: 0 0,10px 10px;
+        }
+
+        tr{
+            background:#eafaf4;
+        }
+ th{
+     background:#0D4C92;
+     text-align:center;
+     color:white;
+     width:10%;
+ }
+ .label-default {
+    background-color: #0D4C92;
+}
+    </style>
+    <script>
+        Swal.fire({
+            title: 'Do you want to save the changes?',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Save',
+            denyButtonText: `Don't save`,
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                Swal.fire('Saved!', '', 'success')
+            } else if (result.isDenied) {
+                Swal.fire('Changes are not saved', '', 'info')
+            }
+        })
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+
+
+         
+
+     
+             <h1 style="text-align:center;">Add New Customer</h1>
+            <br />
+     
+        <div class="container">
+         <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Name</label>
+ 
+        <asp:TextBox ID="TextBox1" runat="server" type="text" class="form-control" placeholder="name" required></asp:TextBox>
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Age</label>
+        <asp:TextBox ID="TextBox2" runat="server" type="number" class="form-control" placeholder="age" required></asp:TextBox>
+    </div>
+  </div>
+                     <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Email</label>
+        <asp:TextBox ID="TextBox3" runat="server" type="Email" class="form-control" placeholder="email" required></asp:TextBox>
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Phone number</label>
+      <asp:TextBox ID="TextBox4" runat="server" type="number" class="form-control" placeholder="Phone" required></asp:TextBox>
+    </div>
+  </div>
+
+    <div class="form-group col-md-6">
+      <label for="inputState">City</label>
+     
+          <asp:DropDownList ID="DropDownList1" runat="server" class="form-control" required></asp:DropDownList>
+          
+    </div>
+             <div class="form-group col-md-6">
+      <label for="inputState" >Upload image</label>
+     
+          
+                             <asp:FileUpload class="form-control" ID="FileUpload1" runat="server" required/>
+
+    </div>
+                   <div class="form-group col-md-6" >
+ 
+     
+          
+                       <asp:Button class="btn btn-primary" ID="Button1" runat="server" Text="Add" OnClick="Button1_Click" />
+
+    </div>
+              </div>
+                
+         
+             <br />
+        <br />
+        <h1 style="text-align:center;">Customers</h1>
+
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+
+            <ContentTemplate>
+                 <div  class="container" style="text-align:center;">
+                                <asp:TextBox  class="form-control form-group col-md-4"  ID="searchTextbox" runat="server"></asp:TextBox>
+<asp:Button class="btn btn-primary" ID="searchButton" Text="Search" runat="server" OnClick="searchButton_Click" />
+
+</div>
+<%--<div class="container">
+                <div class="form-group col-md-6">
+      <label for="inputEmail4">Name</label>
+ 
+        <asp:TextBox ID="TextBox5" runat="server" type="text" class="form-control" placeholder="name" required></asp:TextBox>
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Age</label>
+        <asp:TextBox ID="TextBox6" runat="server" type="number" class="form-control" placeholder="age" required></asp:TextBox>
+    </div>
+</div>--%>
+
+
+          <br />
+                   <div class="container" style="text-align:center;">
+       
+      
+                        <h3 class="form-group col-md-4" >Count<br /><asp:Label ID="Label1" runat="server" Text="Label" class="label label-default"></asp:Label>
+             </h3> 
+                         <h3 class="form-group col-md-4">avarge<br /><asp:Label ID="Label2" runat="server" Text="Label" class="label label-default"></asp:Label>
+             </h3>
+        <h3 class="form-group col-md-4">max<br /><asp:Label ID="Label3" runat="server" Text="Label" class="label label-default"></asp:Label>
+             </h3>
+            </div>
+        <br />
+        
+    
+        <div  class="container">
+            <asp:GridView ID="GridView1"  class="table table-bordered table-condensed table-responsive table-hover  ListGridViewHeader" runat="server" OnRowDataBound="GridView1_RowDataBound" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" >
+            </asp:GridView>
+              <br />
+            <hr />
+      
+         
+        </div>
+           </ContentTemplate>
+        </asp:UpdatePanel>
+     
+    </form>
+  
+</body>
+</html>
---
+++ task1.aspx.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.NETEntityFramework
+{
+    public partial class task1 : System.Web.UI.Page
+    {
+        Entities ww = new Entities();
+        private string fileName;
+
+        public class myclass
+        {
+            public string CustomerIDs { get; set; }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if (!IsPostBack)
+            //{
+            //    var x = from s in ww.Cities select s;
+            //    DropDownList1.DataSource = x.ToList();
+            //    DropDownList1.DataTextField = "CityName";
+            //    DropDownList1.DataValueField = "CityId";
+            //    DropDownList1.DataBind();
+            //}
+
+            //var x1=from s in ww.Cities
+            //       join a in ww.Customers
+            //      on s.CityId equals a.IDcITY
+            //      select new {s.CityName,a.CustomerNames,a.CustomerPhone,a.CustomerEmail,a.CustomerPhoto };
+
+            ////var b = (from p in ww.Customers
+            ////         join e1 in ww.Cities
+            ////         on p.IDcITY equals e1.CityId
+
+            ////         select new
+            ////         {
+            ////            p.CustomerIDs,
+            ////           p.CustomerNames,
+            ////            p.CustomerAges,
+            ////             p.CustomerPhone,
+            ////             p.CustomerEmail,
+            ////               e1.CityName,
+
+            ////             CustomerPhoto7 = p.CustomerPhoto
+            ////         }).ToList();
+
+
+            //GridView1.DataSource = x1.ToList();
+            //GridView1.DataBind();
+
+            //var dataa = ww.Customers.ToList();
+
+
+            //GridView1.DataSource = dataa;
+            //GridView1.DataBind();
+
+            if (!IsPostBack)
+            {
+                var x = from s in ww.Cities select s;
+                DropDownList1.DataSource = x.ToList();
+                DropDownList1.DataTextField = "CityName";
+                DropDownList1.DataValueField = "CityId";
+                DropDownList1.DataBind();
+            }
+
+            var x1 = from s in ww.Cities
+                     join a in ww.Customers
+                     on s.CityId equals a.IDcITY
+                     select new { s.CityName, a.CustomerNames, a.CustomerPhone, a.CustomerEmail, a.CustomerPhoto, a.CustomerAges , a.CustomerIDs /*, ImageUrl = "~/Images/" + a.CustomerPhoto */};
+
+            GridView1.DataSource = x1.ToList();
+            GridView1.DataBind();
+
+            //if (GridView1 != null && GridView1.Columns.Count > 0)
+            //{
+            //    GridView1.Columns[0].HeaderText = "Name";
+            //}
+
+            //Count
+            var count = (from pd in ww.Customers select pd).Count();
+            Label1.Text = count.ToString();
+
+            //Avg
+            var n = ww.Customers.ToList();
+            var avg = n.Average(s => s.CustomerAges);
+            Label2.Text = avg.ToString();
+
+            //Max
+            var max = n.Max(s => s.CustomerAges);
+            Label3.Text = max.ToString();
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            fileName = FileUpload1.FileName;
+            FileUpload1.SaveAs(Server.MapPath("~/Images/") + fileName);
+            //FileUpload1.SaveAs(Server.MapPath("~/Images/") + fileName);
+
+            //Customer insertTest = new Customer();
+            ////Guid guid= Guid.NewGuid();
+            ////insertTest.CustomerNames = guid.ToString();
+            //insertTest.CustomerNames = TextBox1.Text;
+            //insertTest.CustomerAges = Convert.ToInt32(TextBox2.Text);
+            //insertTest.CustomerEmail = TextBox3.Text;
+            //insertTest.CustomerPhone = Convert.ToInt32( TextBox4.Text);
+            //insertTest.IDcITY = int.Parse(DropDownList1.SelectedValue);
+            //insertTest.CustomerCity = (DropDownList1.SelectedValue);
+            //insertTest.CustomerPhoto = FileUpload1.FileName;
+
+
+            //ww.Customers.Add(insertTest);
+            //ww.SaveChanges();
+            //Customer insertTest = new Customer();
+            //insertTest.CustomerNames = TextBox1.Text;
+            //insertTest.CustomerAges = Convert.ToInt32(TextBox2.Text);
+            //insertTest.CustomerEmail = TextBox3.Text;
+            //insertTest.CustomerPhone = Convert.ToInt32(TextBox4.Text);
+            //insertTest.IDcITY = Convert.ToInt32(DropDownList1.SelectedValue);
+            //insertTest.CustomerCity = (DropDownList1.SelectedValue);
+            //insertTest.CustomerPhoto = "~/Images/"+FileUpload1.FileName;
+            //ww.Customers.Add(insertTest);
+            //ww.SaveChanges();
+            Customer insertTest = new Customer();
+            insertTest.CustomerNames = TextBox1.Text;
+            insertTest.CustomerAges = Convert.ToInt32(TextBox2.Text);
+            insertTest.CustomerEmail = TextBox3.Text;
+            insertTest.CustomerPhone = Convert.ToInt32(TextBox4.Text);
+            insertTest.IDcITY = int.Parse(DropDownList1.SelectedValue);
+            insertTest.CustomerCity = (DropDownList1.SelectedValue);
+            insertTest.CustomerPhoto = fileName;
+            ww.Customers.Add(insertTest);
+            ww.SaveChanges();
+            Response.Redirect("/task1.aspx");
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+       
+
+        }
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            //if (GridView1 != null && GridView1.Columns.Count > 0)
+            //{
+            //    GridView1.Columns[0].HeaderText = "Name";
+            //}
+
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                var imagePath = (e.Row.DataItem as dynamic).CustomerPhoto;
+                var image = new Image();
+                image.ImageUrl = "~/Images/" + imagePath;
+                image.Height = 100;
+                image.Width = 100;
+                e.Row.Cells[4].Controls.Add(image);
+            }
+
+           
+        }
+        protected void searchButton_Click(object sender, EventArgs e)
+        {
+            // Get the search query from the textbox
+            string searchQuery = searchTextbox.Text;
+            if (searchQuery != "")
+            {
+                // Use LINQ to filter the data based on the search query
+                var filteredData = from s in ww.Cities
+                                   join a in ww.Customers
+                                   on s.CityId equals a.IDcITY
+                                   where a.CustomerNames.Contains(searchQuery)
+                                   select new { s.CityName, a.CustomerNames, a.CustomerPhone, a.CustomerEmail, a.CustomerPhoto, a.CustomerAges, a.CustomerIDs };
+
+                // Bind the filtered data to the GridView
+                GridView1.DataSource = filteredData.ToList();
+                GridView1.DataBind();
+            }
+        }
+
+
+
+
+    }
+}
+
